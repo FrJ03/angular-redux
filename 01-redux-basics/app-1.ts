01f3ce57d869b7909e219a51e111919ae7171059
FranciscoJoséMelladoOrtiz <franjomor@gmail.com>
2025-06-26 17:28:52 +0200

    feat: counter reducer

--- 01-redux-basics/app-1.ts
+++ 01-redux-basics/app-1.ts
@@ -1,1 +1,50 @@
-console.log("Hola mundo!")
+const ActionType = {
+    INCREMENTAR: 'INCREMENTAR',
+    DECREMENTAR: 'DECREMENTAR',
+    MULTIPLICAR: 'MULTIPLICAR',
+    DIVIDIR: 'DIVIDIR'
+}
+
+interface Action {
+    type: string;
+    payload?: any;
+}
+
+const incrementadorAction: Action = {
+    type: ActionType.INCREMENTAR
+}
+
+const decrementadorAction: Action = {
+    type: ActionType.DECREMENTAR
+}
+
+const multiplicarAction: Action = {
+    type: ActionType.DECREMENTAR,
+    payload: 2
+}
+
+const dividirAction: Action = {
+    type: ActionType.DECREMENTAR,
+    payload: 2
+}
+
+const reducer = (state = 10, action: Action) => {
+    switch (action.type) {
+        case ActionType.INCREMENTAR: 
+            return state++
+
+        case ActionType.DECREMENTAR: 
+            return state--
+
+        case ActionType.MULTIPLICAR:
+            return state * action.payload
+
+        case ActionType.DIVIDIR:
+            return (action.payload === 0) 
+                ? state 
+                : state / action.payload
+
+        default:
+            return state
+    }
+}
