d2dec4e6b27ada9db14b63f7bd361c3cda3eedff
FranciscoJoséMelladoOrtiz <franjomor@gmail.com>
2025-06-27 14:00:08 +0200

    feat: todo angular app init

---
+++ 03-todo-app/src/app/todos/components/todo-add/todo-add.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-add',
+  standalone: false,
+  templateUrl: './todo-add.component.html',
+  styleUrl: './todo-add.component.css'
+})
+export class TodoAddComponent {
+
+}
