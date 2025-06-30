c83b9b4f4b7f352d70c25337dedbe3dddd8252a6
FranciscoJoséMelladoOrtiz <franjomor@gmail.com>
2025-06-30 13:28:36 +0200

    feat: ingreso egreso app initialization

---
+++ 04-ingreso-egreso/src/app/app.routes.ts
@@ -0,0 +1,22 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { dashboardRoutes } from './components/dashboard/dashboard.routes';
+
+export const routes: Routes = [
+    {
+        path: 'login', component: LoginComponent
+    },
+    {
+        path: 'register', component: RegisterComponent
+    },
+    {
+        path: '', component: DashboardComponent,
+        children: dashboardRoutes
+    },
+    {
+        path: '**', component: DashboardComponent,
+        children: dashboardRoutes
+    }
+];
---
+++ 04-ingreso-egreso/src/app/components/ingreso-egreso/estadistica/estadistica.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-estadistica',
+  imports: [],
+  templateUrl: './estadistica.component.html'
+})
+export class EstadisticaComponent {
+
+}
