30a23417ac4b7db45cfbfe7f3689e503f8656852
FranciscoJoséMelladoOrtiz <franjomor@gmail.com>
2025-07-01 14:02:50 +0200

    feat: set username in sidebar

--- 04-ingreso-egreso/src/app/components/shared/sidebar/sidebar.component.ts
+++ 04-ingreso-egreso/src/app/components/shared/sidebar/sidebar.component.ts
@@ -1,17 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../../services/auth.service';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../../reducers/app.reducer';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-sidebar',
   imports: [RouterLink],
   templateUrl: './sidebar.component.html'
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnDestroy{
+  username: string = ''
+  storeSubscription: Subscription
+
   constructor(
     private auth: AuthService,
-    private router: Router
-  ){}
+    private router: Router,
+    private store: Store<AppState>
+  ){
+    this.storeSubscription = this.store.select(store => store.user.user).subscribe(user => {
+      if(user){
+        this.username = user.username
+      }
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.storeSubscription.unsubscribe()
+  }
 
   logout(){
     this.auth.logout()
