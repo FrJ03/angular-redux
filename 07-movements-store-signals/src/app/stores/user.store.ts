ddf27b7260327d8eedaeb7b7e0cdfced3b69a0f6
FranciscoJoséMelladoOrtiz <franjomor@gmail.com>
2025-07-07 11:57:25 +0200

    refactor: declare user store methods separatelly

--- 07-movements-store-signals/src/app/stores/user.store.ts
+++ 07-movements-store-signals/src/app/stores/user.store.ts
@@ -2,6 +2,7 @@ import { inject } from "@angular/core"
 import { signalStore, withMethods, withState, patchState } from "@ngrx/signals"
 import { AuthService } from "../services/auth.service"
 import { User } from "../models/user.model"
+import { checkLogged, logout, logUser, saveUser } from "../methods/user.methods"
 
 export interface UserState {
     user: User | null,
@@ -21,123 +22,11 @@ export const UserStore = signalStore(
     withMethods(store => {
         const authService = inject(AuthService)
         return {
-            async saveUser(user: User): Promise<void> {
-                patchState(store, state => ({
-                    ...state,
-                    loading: true,
-                    error: null
-                }))
-
-                const result = await authService.createUser(user)
-
-                if(result.success){
-                    const logResult = await authService.loginUser(
-                        user.email,
-                        user.password
-                    )
-
-                    if(logResult.result?.success) {
-                        patchState(store, state => ({
-                            ...state,
-                            loading: false,
-                            user: user
-                        }))
-                    } else {
-                        patchState(store, state => ({
-                            ...state,
-                            loading: false,
-                            error: logResult.result?.message ?? 'Login error'
-                        }))
-                    }
-                } else {
-                    patchState(store, state => ({
-                        ...state,
-                        loading: false,
-                        error: result.message
-                    }))
-                }
-            },
-            async logUser(email: string, password: string): Promise<void>{
-                patchState(store, state => ({
-                    ...state,
-                    loading: true,
-                    error: null
-                }))
-
-                const result = await authService.loginUser(email, password)
-
-                if(result.result?.success && result.user){
-                    patchState(store, state => ({
-                        ...state,
-                        loading: false,
-                        user: result.user
-                    }))
-                } else {
-                    patchState(store, state => ({
-                        ...state,
-                        loading: false,
-                        error: result.result?.message ?? 'Loading error'
-                    }))
-                }
-            },
-            async checkLogged(): Promise<void>{
-                patchState(store, state => ({
-                    ...state,
-                    loading: true,
-                    error: null
-                }))
-                
-                const result = await authService.checkLogged()
-
-                if(result.success && result.user) {
-                    const logResult = await authService.loginUser(
-                        result.user.email,
-                        result.user.password
-                    )
-
-                    if(logResult.result?.success) {
-                        patchState(store, state => ({
-                            ...state,
-                            loading: false,
-                            user: result.user
-                        }))
-                    } else {
-                        patchState(store, state => ({
-                            ...state,
-                            loading: false,
-                            error: logResult.result?.message ?? 'Login error'
-                        }))
-                    }
-                } else {
-                    patchState(store, state => ({
-                        ...state,
-                        loading: false,
-                        error: result.message
-                    }))
-                }
-            },
-            async logout(): Promise<void> {
-                patchState(store, state => ({
-                    ...state,
-                    loading: true,
-                    error: null
-                }))
-                const result = await authService.logout()
-
-                if(result.success) {
-                    patchState(store, state => ({
-                        ...state,
-                        loading: false,
-                        user: null
-                    }))
-                } else {
-                    patchState(store, state => ({
-                        ...state,
-                        loading: false,
-                        error: result.message
-                    }))
-                }
-            }
+            saveUser: (user: User) => saveUser(store, user, authService),
+            logUser: (email: string, password: string) => 
+                logUser(store, email, password, authService),
+            checkLogged: () => checkLogged(store, authService),
+            logout: () => logout(store, authService)
         }
     })
 )
